6667012f65b663df39a43c09af9023afc944aba4
FlickeringLamp <3454877998@qq.com>
2025-10-24 17:01:42 +0800

    修复前端index拓展文件名

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,9 +17,11 @@ export default defineConfig({
   define: {
     'process.env': {}
   },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: []
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
   }
 })
